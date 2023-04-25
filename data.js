8cef10de0abdf89278ecc3c21db5136662c4d11a
ymazi <131825914+ymazi@users.noreply.github.com>
2023-04-25 23:33:14 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,122 @@
+
+    let lessons = [
+      {
+        "id": 100,
+        "subjectImg": "mathsImg.png",
+        "subjectName": "Physics",
+        "location": "New York",
+        "price": 23,
+        "spaces": 6,
+        "currency": "$"
+      },
+      {
+        "id": 101,
+        "subjectImg": "mathsImg.png",
+        "subjectName": "Mathematics",
+        "location": "Los Angeles",
+        "price": 17,
+        "spaces": 9,
+        "currency": "$"
+      },
+      {
+        "id": 102,
+        "subjectImg": "mathsImg.png",
+        "subjectName": "Science",
+        "location": "Chicago",
+        "price": 35,
+        "spaces": 3,
+        "currency": "$"
+      },
+      {
+        "id": 103,
+        "subjectImg": "mathsImg.png",
+        "subjectName": "Biology",
+        "location": "Houston",
+        "price": 28,
+        "spaces": 12,
+        "currency": "$"
+      },
+      {
+        "id": 104,
+        "subjectImg": "mathsImg.png",
+        "subjectName": "Chemistry",
+        "location": "Philadelphia",
+        "price": 12,
+        "spaces": 8,
+        "currency": "$"
+      },
+      {
+        "id": 105,
+        "subjectImg": "mathsImg.png",
+        "subjectName": "History",
+        "location": "Phoenix",
+        "price": 20,
+        "spaces": 5,
+        "currency": "$"
+      },
+      {
+        "id": 106,
+        "subjectImg": "mathsImg.png",
+        "subjectName": "Geography",
+        "location": "San Antonio",
+        "price": 15,
+        "spaces": 2,
+        "currency": "$"
+      },
+      {
+        "id": 107,
+        "subjectImg": "mathsImg.png",
+        "subjectName": "English",
+        "location": "San Diego",
+        "price": 18,
+        "spaces": 7,
+        "currency": "$"
+      },
+      {
+        "id": 108,
+        "subjectImg": "mathsImg.png",
+        "subjectName": "French",
+        "location": "Dallas",
+        "price": 25,
+        "spaces": 10,
+        "currency": "$"
+      },
+      {
+        "id": 109,
+        "subjectImg": "mathsImg.png",
+        "subjectName": "Spanish",
+        "location": "San Jose",
+        "price": 21,
+        "spaces": 4,
+        "currency": "$"
+      },
+      {
+        "id": 110,
+        "subjectImg": "mathsImg.png",
+        "subjectName": "German",
+        "location": "Austin",
+        "price": 19,
+        "spaces": 8,
+        "currency": "$"
+      },
+      {
+        "id": 111,
+        "subjectImg": "mathsImg.png",
+        "subjectName": "Italian",
+        "location": "Jacksonville",
+        "price": 20,
+        "spaces": 3,
+        "currency": "$"
+      },
+      {
+        "id": 112,
+        "subjectImg": "mathsImg.png",
+        "subjectName": "Russian",
+        "location": "Indianapolis",
+        "price": 22,
+        "spaces": 6,
+        "currency": "$"
+      },
+    ]
+  ;
+  
